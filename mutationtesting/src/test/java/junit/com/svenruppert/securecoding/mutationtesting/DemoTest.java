783dc2a1af2d9abaab02158cfd5a675f56086d4b
Sven Ruppert <sven.ruppert@gmail.com>
2025-06-04 14:15:51 +0200

    start mutationtesting

---
+++ mutationtesting/src/test/java/junit/com/svenruppert/securecoding/mutationtesting/DemoTest.java
@@ -0,0 +1,15 @@
+package junit.com.svenruppert.securecoding.mutationtesting;
+
+import com.svenruppert.securecoding.mutationtesting.Demo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class DemoTest {
+
+  @Test
+  void test001() {
+    var demo = new Demo();
+//    int result = demo.add(x, x);
+  }
+}
